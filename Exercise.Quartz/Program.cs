de414a171fa4f9ee15d172b0cac9608e5dd468db
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-04-29 21:39:38 +0200

    Add simple example

--- Exercise.Quartz/Program.cs
+++ Exercise.Quartz/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Exercise.Quartz.Jobs;
+using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercise.Quartz
@@ -10,6 +14,31 @@ namespace Exercise.Quartz
     {
         static void Main(string[] args)
         {
+            ISchedulerFactory factory = new StdSchedulerFactory();
+
+            IScheduler scheduler = factory.GetScheduler().Result;
+
+            IJobDetail job = JobBuilder.Create<ConsoleJob>()
+                                .WithIdentity("jobName", "JobGroup")
+                                .UsingJobData("Name","JobDataName")
+                                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                                .WithSimpleSchedule(a => a.WithIntervalInSeconds(2).WithRepeatCount(5))
+                                .StartNow()
+                                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+
+            scheduler.Start();
+
+            Thread.Sleep(TimeSpan.FromMinutes(1));
+
+            scheduler.Shutdown();
+
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
         }
     }
 }
